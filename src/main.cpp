7e037ab5162aa1b5fb524f94c20092f25aab2212
Eva Fiedler <evamfied@gmail.com>
2024-02-07 15:21:40 -0800

    Adapted code for new current sensors

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-#include <Adafruit_INA260.h>
-
-Adafruit_INA260 ina260 = Adafruit_INA260();
+// #include <Adafruit_INA260.h>
+#include <Arduino.h>
+// Adafruit_INA260 ina260 = Adafruit_INA260();
 
 enum STATES {
   initState,
@@ -9,11 +9,18 @@ enum STATES {
   loadDisconnect
 };
 
+// Pin for current sensor reading
+const int currentSensor = A0;
+const float VOLTAGE_REF = 5.0; // Volts
+const float RESISTANCE = 30.0; // Ohms
+
 const int relaySetSignal = 12;
 const int relayResetSignal = 13;
 
 STATES state;
 int relayState; // 0 for load, 1 for PCC
+
+float readCurrent();
 
 void switchRelayToLoad();
 void switchRelayToPCC();
@@ -21,14 +28,14 @@ void switchRelayToPCC();
 void initialize();
 void normalOperation();
 void emergencyStop();
-void loadDisconection();
+void loadDisconnection();
 
 void setup() {
   Serial.begin(9600);
 
   while (!Serial) {delay(10);}
   
-  ina260.begin();
+  //ina260.begin();
 
   pinMode (7, OUTPUT);
   pinMode(relaySetSignal, OUTPUT);
@@ -58,6 +65,12 @@ void loop() {
   }
 }
 
+// Helper function for reading current in Amps
+float readCurrent() {
+  float current = (analogRead(currentSensor) * VOLTAGE_REF) / 1023;
+  return current;
+}
+
 void switchRelayToLoad() {      
   digitalWrite(relaySetSignal, HIGH);
   delay(5);
@@ -72,13 +85,14 @@ void switchRelayToPCC() {
   relayState = 1;
 }
 
+// TODO: check this logic (not immediately setting to PCC)
+// I think this requires that load is always connected and turned on first
 void initialize() {
   switchRelayToLoad();
   delay(10);
-  //ina260.begin();
-  float voltage = ina260.readBusVoltage();
+  float voltage = readCurrent() * RESISTANCE; // voltage in V
 
-  if (voltage < 7000) {
+  if (voltage < 7) {
     // turbine side arduino not receiving enough power
     switchRelayToPCC();
     while (Serial.available() <= 0) {
@@ -86,38 +100,37 @@ void initialize() {
     }
     char c = Serial.read();
     if (c == 'O') {
+      // Turbine side generating it's own power
       switchRelayToLoad();
     }
   }
   state = normalOp;
 }
 
-void normalOperation() {
-  digitalWrite(7, LOW);
-  //ina260.begin();
-    // Power and voltage readings
-  float power = ina260.readPower(); //power in mW
-  float voltage = ina260.readBusVoltage() / 1000; //voltage in V
+void normalOperation() {   
+  digitalWrite(7, LOW); // TODO: what is this line for???
+
+  // Power and voltage readings
+  float current = readCurrent();
+  float voltage = current * RESISTANCE; // voltage in V 
+  float power = current * voltage; // power in W
   //Serial.println(power);
+
   char c;
   if (Serial.available() > 0) {
     c = Serial.read();
-    //Serial.println("char read");
+
     if (c == 'E') {
       state = eStop;
       switchRelayToPCC();
     }
   }
 
-  if (relayState == 0 && voltage < 1){ //arbitrarilly low threshold for load disconnected voltage (determine experimentally)
+  if (relayState == 0 && voltage < 0.1){ //arbitrarilly low threshold for load disconnected voltage (TODO: determine experimentally)
     state = loadDisconnect;
     Serial.print('D');
     switchRelayToPCC();
-  }
-
-//  if (relayState == 1) {
-//    switchRelayToLoad();
-//  }
+  } 
 }
 
 void emergencyStop() {
@@ -134,11 +147,14 @@ void emergencyStop() {
   }
 }
 
+// Pretty sure the logic for this is messed up -- ask Alyssa for her copies
+// I think you just need to wait for 'O' (possible switch to 'N' as that now indicates moving into normal mode)
 void loadDisconnection() {
   char c;
   if (Serial.available()) {
     c = Serial.read();
     if (c == 'O') {
+      // TODO: in Turbine Controls I don't think we handle this case???
       Serial.print('R');
     }
 
